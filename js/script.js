61231e8d6d25b89ccd39571963d10c88f1809341
Misha Evangelista <89716798+b3stg1rlm1sh4@users.noreply.github.com>
2021-08-30 01:24:21 +0000

    Implemented timer and end screen

--- js/script.js
+++ js/script.js
@@ -1,13 +1,28 @@
-const gameTitle = document.getElementById("main-container-title");
+const scoreDisplay = document.getElementById("score-display");
+const timeDisplay = document.getElementById("time-display");
 const blocks = [document.getElementById("btn1"), document.getElementById("btn2"), document.getElementById("btn3"), document.getElementById("btn4"), document.getElementById("btn5"), document.getElementById("btn6"), document.getElementById("btn7"), document.getElementById("btn8"), document.getElementById("btn9")]
 
 let green;
 let score = 0;
+let time = 30;
+let startTimer = false;
 let gotGreen = false;
 gLoc = Math.floor(Math.random() * 9);
 
 const updater = setInterval(() => {
-  gameTitle.textContent = `Score: ${score}`;
+  scoreDisplay.textContent = `Score: ${score}`;
+  timeDisplay.textContent = `Time: ${time}`;
+  if(time == 0) {
+    blocks.forEach(function(item) {
+      item.textContent = "X";
+      item.style.backgroundColor = "#3df58a";
+      item.style.color = "#ffffff";
+      item.disabled = true;
+    })
+    clearInterval(timeUpdate);
+    clearInterval(game);
+    clearInterval(updater);
+  }
 }, 5)
 
 function updateBoard(){
@@ -23,12 +38,19 @@ function updateBoard(){
   green.style.color = "#ffffff";
   green.addEventListener("click", function restart() {
     score++;
+    startTimer = true;
     gotGreen = true;
     green.removeEventListener("click", restart);
   })
 }
 
 updateBoard()
+
+const timeUpdate = setInterval(() => {
+  if(startTimer) {
+    time--;
+  }
+}, 1000);
 
 const game = setInterval(() => {
   if(gotGreen) {
